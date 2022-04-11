5aee986160f111f42ae6f25f920ff47a03e6f02b
MhmdHabbal <mhmdhabbal2001@gmail.com>
2022-04-11 19:06:12 +0300

    This is the signup javascript

---
+++ Signup.js
@@ -0,0 +1,6 @@
+document.getElementById('signup-btn').onclick = function() {
+    location.href="SignupForm.html";
+}
+document.getElementById('signin-btn').onclick = function() {
+    location.href="SigninForm.html";
+}
